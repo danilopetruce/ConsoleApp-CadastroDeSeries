f83d8dc24be726c07b3dcd5bbaf6671e43544174
Danilo Petruce Marques <danilopetruce@hotmail.com>
2021-04-30 18:32:54 -0300

    Metodo de exclusão e visualização implementados com sucesso

--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -5,34 +5,33 @@ namespace DAN.Series
 {
   public class Serie : EntidadeBase
   {
+    // Atributos
     private Genero Genero { get; set; }
     private string Titulo { get; set; }
     private string Descricao { get; set; }
     private int Ano { get; set; }
     private bool Excluido { get; set; }
 
-    public Serie(int Id, Genero genero, string titulo, string descricao, int ano)
+    // Métodos
+    public Serie(int id, Genero genero, string titulo, string descricao, int ano)
     {
-      this.Id = Id;
+      this.Id = id;
       this.Genero = genero;
       this.Titulo = titulo;
       this.Descricao = descricao;
       this.Ano = ano;
       this.Excluido = false;
-
     }
 
     public override string ToString()
     {
       string retorno = "";
       retorno += "Gênero: " + this.Genero + Environment.NewLine;
-      retorno += "Título: " + this.Titulo + Environment.NewLine;
+      retorno += "Titulo: " + this.Titulo + Environment.NewLine;
       retorno += "Descrição: " + this.Descricao + Environment.NewLine;
-      retorno += "Ano: " + this.Ano;
+      retorno += "Ano de Início: " + this.Ano + Environment.NewLine;
       retorno += "Excluido: " + this.Excluido;
-
       return retorno;
-
     }
 
     public string retornaTitulo()
@@ -44,7 +43,10 @@ namespace DAN.Series
     {
       return this.Id;
     }
-
+    public bool retornaExcluido()
+    {
+      return this.Excluido;
+    }
     public void Excluir()
     {
       this.Excluido = true;
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using DAN.Series.Classes;
 
+
 namespace DAN.Series
 {
   class Program
@@ -21,13 +22,13 @@ namespace DAN.Series
             InserirSerie();
             break;
           case "3":
-            EditarSerie();
+            AtualizarSerie();
             break;
           case "4":
-            //ExcluirSerie();
+            ExcluirSerie();
             break;
           case "5":
-            // VisualizarSerie();
+            VisualizarSerie();
             break;
           case "C":
             Console.Clear();
@@ -35,83 +36,108 @@ namespace DAN.Series
 
           default:
             throw new ArgumentOutOfRangeException();
-
         }
 
         opcaoUsuario = ObterOpcaoUsuario();
-
       }
 
-      Console.WriteLine("Obrigado por utilizar nossos serviços");
+      Console.WriteLine("Obrigado por utilizar nossos serviços.");
       Console.ReadLine();
-
     }
-    private static void EditarSerie()
+
+    private static void ExcluirSerie()
     {
       Console.Write("Digite o id da série: ");
       int indiceSerie = int.Parse(Console.ReadLine());
+
+      repositorio.Exclui(indiceSerie);
+    }
+
+    private static void VisualizarSerie()
+    {
+      Console.Write("Digite o id da série: ");
+      int indiceSerie = int.Parse(Console.ReadLine());
+
+      var serie = repositorio.RetornaPorId(indiceSerie);
+
+      Console.WriteLine(serie);
+    }
+
+    private static void AtualizarSerie()
+    {
+      Console.Write("Digite o id da série: ");
+      int indiceSerie = int.Parse(Console.ReadLine());
+
 
       foreach (int i in Enum.GetValues(typeof(Genero)))
       {
         Console.WriteLine("{0}-{1}", i, Enum.GetName(typeof(Genero), i));
       }
-
       Console.Write("Digite o gênero entre as opções acima: ");
       int entradaGenero = int.Parse(Console.ReadLine());
 
       Console.Write("Digite o Título da Série: ");
       string entradaTitulo = Console.ReadLine();
 
-      Console.Write("Digite o Ano de Inicio da Série: ");
+      Console.Write("Digite o Ano de Início da Série: ");
       int entradaAno = int.Parse(Console.ReadLine());
 
       Console.Write("Digite a Descrição da Série: ");
       string entradaDescricao = Console.ReadLine();
 
-      Serie atualizaSerie = new Serie(Id: indiceSerie, genero: (Genero)entradaGenero,
-                                    titulo: entradaTitulo, ano: entradaAno, descricao: entradaDescricao);
-
+      Serie atualizaSerie = new Serie(id: indiceSerie,
+                    genero: (Genero)entradaGenero,
+                    titulo: entradaTitulo,
+                    ano: entradaAno,
+                    descricao: entradaDescricao);
 
       repositorio.Atualiza(indiceSerie, atualizaSerie);
     }
-
     private static void ListarSeries()
     {
       Console.WriteLine("Listar séries");
+
       var lista = repositorio.Lista();
 
       if (lista.Count == 0)
       {
-        Console.WriteLine("Nenhuma série cadastrada");
+        Console.WriteLine("Nenhuma série cadastrada.");
         return;
       }
+
       foreach (var serie in lista)
       {
-        Console.WriteLine("#ID {0}: - {1}", serie.retornaId(), serie.retornaTitulo());
+        var excluido = serie.retornaExcluido();
+
+        Console.WriteLine("#ID {0}: - {1} {2}", serie.retornaId(), serie.retornaTitulo(), (excluido ? "*Excluído*" : ""));
       }
     }
 
     private static void InserirSerie()
     {
-      Console.WriteLine("Inserir Série");
+      Console.WriteLine("Inserir nova série");
+
       foreach (int i in Enum.GetValues(typeof(Genero)))
       {
-        Console.WriteLine("{0} - {1}", i, Enum.GetName(typeof(Genero), i));
+        Console.WriteLine("{0}-{1}", i, Enum.GetName(typeof(Genero), i));
       }
-      Console.Write("Digite o Gênero entre as opções acima:");
+      Console.Write("Digite o gênero entre as opções acima: ");
       int entradaGenero = int.Parse(Console.ReadLine());
 
-      Console.Write("Digite o Título da Série:");
-      string entradaTitulo = (Console.ReadLine());
+      Console.Write("Digite o Título da Série: ");
+      string entradaTitulo = Console.ReadLine();
 
-      Console.Write("Digite o Ano de Inicio da Série:");
+      Console.Write("Digite o Ano de Início da Série: ");
       int entradaAno = int.Parse(Console.ReadLine());
 
-      Console.Write("Digite a Descrição da Série");
-      string entradaDescricao = (Console.ReadLine());
+      Console.Write("Digite a Descrição da Série: ");
+      string entradaDescricao = Console.ReadLine();
 
-      Serie novaSerie = new Serie(Id: repositorio.ProximoId(), genero: (Genero)entradaGenero, titulo: entradaTitulo,
-        ano: entradaAno, descricao: entradaDescricao);
+      Serie novaSerie = new Serie(id: repositorio.ProximoId(),
+                    genero: (Genero)entradaGenero,
+                    titulo: entradaTitulo,
+                    ano: entradaAno,
+                    descricao: entradaDescricao);
 
       repositorio.Insere(novaSerie);
     }
@@ -119,29 +145,22 @@ namespace DAN.Series
     private static string ObterOpcaoUsuario()
     {
       Console.WriteLine();
-      Console.WriteLine("################### DIO SERIES ###################");
+      Console.WriteLine(" ########################### DAN SERIES ###########################");
+      Console.WriteLine();
       Console.WriteLine("Informe a opção desejada:");
 
-      Console.WriteLine("1 - Listar Séries");
-      Console.WriteLine("2 - Inserir Nova Série");
-      Console.WriteLine("3 - Atualizar Série");
-      Console.WriteLine("4 - Excluir Série");
-      Console.WriteLine("5 - Vizualizar Série");
-      Console.WriteLine("C - Limpar Tela ");
-      Console.WriteLine("X - Sair");
+      Console.WriteLine("1- Listar séries");
+      Console.WriteLine("2- Inserir nova série");
+      Console.WriteLine("3- Atualizar série");
+      Console.WriteLine("4- Excluir série");
+      Console.WriteLine("5- Visualizar série");
+      Console.WriteLine("C- Limpar Tela");
+      Console.WriteLine("X- Sair");
       Console.WriteLine();
 
       string opcaoUsuario = Console.ReadLine().ToUpper();
       Console.WriteLine();
       return opcaoUsuario;
-
-
-
-
-
-
-
-
     }
   }
 }
